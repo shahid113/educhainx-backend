42cb36519a420ead08b16ef2bcdebb6c961f6f19
Shahid <shahidwazidh@gmail.com>
2025-10-17 22:11:20 +0530

    fix cors

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ const verifierRoutes = require('./routes/verifierRoutes');
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
+  origin: (origin, callback) => {
+    callback(null, true); // allow all origins dynamically
+  },
+  credentials: true, // allow cookies/authorization headers
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
